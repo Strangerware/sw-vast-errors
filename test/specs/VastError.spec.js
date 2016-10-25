af465e6d91af6da30d780bd7945e1ece0dedb478
Marco Massarotto <massa84@gmail.com>
2016-10-25 22:53:46 +0100

    test: add VastError tests

---
+++ test/specs/VastError.spec.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+import errorDescriptions from '../../src/errorDescriptions';
+import VastError from '../../src/VastError';
+
+const defaultErrorCode = 900;
+
+test('must resolve the correct description for the provided errorCode', (t) => {
+  const errorCode = 400;
+  const error = new VastError({ errorCode });
+  t.is(error.errorCode, errorCode);
+  t.is(error.errorDescription, errorDescriptions[errorCode]);
+});
+
+test('must use the default errorCode if none is provided', (t) => {
+  const error = new VastError();
+  t.is(error.errorCode, defaultErrorCode);
+  t.is(error.message, errorDescriptions[defaultErrorCode]);
+});
+
+test('must allow for custom errors', (t) => {
+  const error = new VastError({ errorCode: 666, errorDescription: 'custom error' });
+  t.is(error.errorCode, 666);
+  t.is(error.message, 'custom error');
+});
+
+test('must store custom data', (t) => {
+  const error = new VastError({ data: { customValue: 1 } });
+  t.deepEqual(error.data, { customValue: 1 });
+});
+
+test('must serialize correctly', (t) => {
+  const error = new VastError();
+  const clone = JSON.parse(JSON.stringify(error));
+  t.deepEqual(Object.keys(clone), ['errorCode', 'errorDescription', 'stack']);
+});
+
+test('must log correctly', (t) => {
+  const error = new VastError();
+  t.is(`${error}`, `Vast Error ${defaultErrorCode}: ${errorDescriptions[defaultErrorCode]}`);
+});
